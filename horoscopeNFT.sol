a74bd04678641f1c16a1b71a98f1ab167daadcdb
Aamer Mershed <116386034+AamerMershed@users.noreply.github.com>
2023-02-09 08:58:24 +0100

    Update horoscopeNFT.sol

--- horoscopeNFT.sol
+++ horoscopeNFT.sol
@@ -10,7 +10,7 @@ contract horoscopeNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Horoscope", "HSCO") {}
+    constructor() ERC721("Horoscope", "HSCP") {}
 
     function mintNFT(address recipient, string memory zodiacSign) 
         
